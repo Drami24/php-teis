a68872a7469f6f6df85bbc0d29865d9f886ab765
damianld <damilores@gmail.com>
2019-10-13 05:04:07 +0200

    Paso por Referencia

--- 4PasorPorReferencia.php
+++ 4PasorPorReferencia.php
@@ -3,10 +3,10 @@
 $numero = 4;
 echo 'Valor de $numero antes da función: ' . $numero . '<br/>';
 
-function sumarUn($numero) {
+function sumarUn(&$numero) {
     $numero += 1;
     echo 'Dentro da función $numero é ' . $numero;
 }
 sumarUn($numero);
 
-echo '<br/>Fora da función $numero é ' . $numero;
+echo '<br/>Fora da función $numero é ' . $numero; 
