a0e0bd9f3e270253d609ac811833d3a3dea9e657
damianld <damilores@gmail.com>
2019-10-14 18:29:12 +0200

    calcular area

---
+++ 25CalcularArea.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Areas</title>
+</head>
+<body>
+    <h1>Calcular área</h1>
+    <form action="" method="post">
+        <fieldset>
+            <h3>Círculo</h2>
+            <label for="radio">Radio</label>
+            <input type="text" name="radio" id="radio">
+        </fieldset>
+        <fieldset>
+            <h3>Triángulo</h3>
+            <label for="base">Base</label>
+            <input type="text" name="base" id="base">
+            <label for="altura">Base</label>
+            <input type="text" name="altura" id="altura">
+        </fieldset>
+        <fieldset>
+            <h3>Cuadrado</h3>
+        </fieldset>
+        <div>
+            <label for="base">Base</label>
+            <input type="text" name="base" id="base">
+        </div>
+        <input type="radio" name="area" id="circulo">Circulo <br/>
+        <input type="radio" name="area" id="triangulo">Triangulo <br/>
+        <input type="radio" name="area" id="cuadrado">Cuadrado
+
+    </form>
+</body>
+</html> 
