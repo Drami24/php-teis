472ee9aac9e05521308c774ce3b685bee4fddd75
damianld <damilores@gmail.com>
2019-10-16 17:39:15 +0200

    calcular areas

--- 25CalcularArea.php
+++ 25CalcularArea.php
@@ -1,36 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Areas</title>
-</head>
-<body>
-    <h1>Calcular área</h1>
-    <form action="" method="post">
-        <fieldset>
-            <h3>Círculo</h2>
-            <label for="radio">Radio</label>
-            <input type="text" name="radio" id="radio">
-        </fieldset>
-        <fieldset>
-            <h3>Triángulo</h3>
-            <label for="base">Base</label>
-            <input type="text" name="base" id="base">
-            <label for="altura">Base</label>
-            <input type="text" name="altura" id="altura">
-        </fieldset>
-        <fieldset>
-            <h3>Cuadrado</h3>
-        </fieldset>
-        <div>
-            <label for="base">Base</label>
-            <input type="text" name="base" id="base">
-        </div>
-        <input type="radio" name="area" id="circulo">Circulo <br/>
-        <input type="radio" name="area" id="triangulo">Triangulo <br/>
-        <input type="radio" name="area" id="cuadrado">Cuadrado
+<?php
 
-    </form>
-</body>
-</html> 
+$area = $_POST['area'];
+if (isset($area) && $area=="circulo") {
+    $radio = $_POST['radio'];
+    if (isset($radio) && validarNumero($radio)) {
+        echo 'El área del circulo es ' . calcularAreaCirculo($radio);
+    } else {
+        echo 'No has introducido un número válido';
+    }
+}
+
+function validarNumero($numero) {
+    if ($numero > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calcularAreaCirculo($radio) {
+    $area = pi() * pow($radio, 2);
+    $area = round($area, 2);
+    return $area;
+}
+
+function calcularAreaTriangulo() {
+
+}
+
+function calcularAreaCuadrado() {
+
+}
