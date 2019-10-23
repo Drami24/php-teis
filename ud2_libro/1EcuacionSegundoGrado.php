29b4afadcace44f5a24b93997d944d6883dfd91a
damianld <damilores@gmail.com>
2019-10-23 17:07:36 +0200

    añadir round

--- ud2_libro/1EcuacionSegundoGrado.php
+++ ud2_libro/1EcuacionSegundoGrado.php
@@ -8,8 +8,8 @@ $solucion1 = (-$b + sqrt(pow($b,2) - 4 * $a * $c)) / (2 * $a);
 $solucion2 = (-$b - sqrt(pow($b,2) - 4 * $a * $c)) / (2 * $a);
 
 if (!is_nan($solucion1) && !is_nan($solucion2)) {
-    echo 'Solución 1: ' . $solucion1 . '<br/>';
-    echo 'Solución 2: ' . $solucion2 . '<br/>';
+    echo 'Solución 1: ' . round($solucion1, 2) . '<br/>';
+    echo 'Solución 2: ' . round($solucion2, 2) . '<br/>';
 } else {
     echo 'La ecuación no tiene soluciones reales.';
 }
