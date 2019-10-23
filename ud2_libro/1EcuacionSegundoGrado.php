b7d74f23563658b4582ddb54c084f64f3b855124
damianld <damilores@gmail.com>
2019-10-23 16:41:25 +0200

    ejercicio segundo grado resolto

--- ud2_libro/1EcuacionSegundoGrado.php
+++ ud2_libro/1EcuacionSegundoGrado.php
@@ -1,12 +1,15 @@
 <?php
 
-$a = 4;
-$b = 8;
-$c = 3;
+$a = 2;
+$b = 5;
+$c = 2;
 
-$solucion1 = (-$b + sqrt(pow($b,2) - $a * $c)) / 2 * $a;
-$solucion2 = (-$b - sqrt(pow($b,2) - $a * $c)) / 2 * $a;
+$solucion1 = (-$b + sqrt(pow($b,2) - 4 * $a * $c)) / (2 * $a);
+$solucion2 = (-$b - sqrt(pow($b,2) - 4 * $a * $c)) / (2 * $a);
 
-echo $solucion1 . '<br/>';
-echo $solucion2;
-
+if (!is_nan($solucion1) && !is_nan($solucion2)) {
+    echo 'Solución 1: ' . $solucion1 . '<br/>';
+    echo 'Solución 2: ' . $solucion2 . '<br/>';
+} else {
+    echo 'La ecuación no tiene soluciones reales.';
+}
