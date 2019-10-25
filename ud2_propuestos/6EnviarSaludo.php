27263b491467f96e0710ec01bbb79474f944b984
damianld <damilores@gmail.com>
2019-10-25 17:39:03 +0200

    subir cambios

---
+++ ud2_propuestos/6EnviarSaludo.php
@@ -0,0 +1,12 @@
+<?php
+
+$hora = $_POST['hora'];
+
+switch ($hora) {
+    case 1:
+    case 2:
+    case 3:
+    
+        echo 'buenas madrugadas';
+        break;
+}
---
+++ ud2_propuestos/6HoraPorTeclado.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>6</title>
+</head>
+<body>
+    <h2>Hora por teclado</h2>
+    <form action="6EnviarSaludo.php" method="post">
+        <label for="hora">Hora (1-24)</label>
+        <input type="text" name="hora" id="hora" placeholder='Introduzca una hora'>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
