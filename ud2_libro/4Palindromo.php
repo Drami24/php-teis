9435df80a041b29d8da57b0e60afa2a5822ad455
damianld <damilores@gmail.com>
2019-10-25 02:33:40 +0200

    mejorado

--- ud2_libro/4Palindromo.php
+++ ud2_libro/4Palindromo.php
@@ -1,12 +1,14 @@
 <?php
 
-$texto = 'luz azul';
+$texto = 'Luz azul';
 
 function esPalindromo($texto) {
+    $texto = str_replace(',', ' ', $texto);
     $texto = str_replace(' ', '', $texto);
-    $textoInvertido = strrev(str_replace(' ', '', $texto));
-    echo $textoInvertido . '<br/>'; 
-    if(strrev($textoInvertido) == $texto) {
+    $texto = strtolower($texto);
+    $texto = quitar_tildes($texto);
+    $textoInvertido = strrev($texto);
+    if($textoInvertido == $texto) {
         return true;
     } else {
         return false;
@@ -15,10 +17,23 @@ function esPalindromo($texto) {
 
 function mostrarPalindromo($texto) {
     if (esPalindromo($texto) == true) {
-        echo $texto . ' es palíndromo.';
+        echo "<b>$texto</b> es palíndromo.<br/>";
     } else {
-        echo $texto . ' NO es palíndromo';
+        echo "<b>$texto</b> NO es palíndromo<br/>";
     }
 }
 
+function quitar_tildes($cadena) {
+    $no_permitidas= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
+    $permitidas= array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E");
+    $texto = str_replace($no_permitidas, $permitidas ,$cadena);
+    return $texto;
+}
+
 mostrarPalindromo($texto);
+mostrarPalindromo('No subas, abusón');
+mostrarPalindromo('Yo dono rosas, oro no doy');
+mostrarPalindromo('Adán no cede con Eva y Yavé no cede con nada');
+mostrarPalindromo('O rey, o joyero');
+mostrarPalindromo('Arroz a la dama da la zorra');
+mostrarPalindromo('Esto no es');
