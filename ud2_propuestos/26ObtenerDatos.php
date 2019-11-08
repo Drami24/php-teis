fe2b6c07968cb96d8dabdd5e5e060582f7f1cea4
damianld <damilores@gmail.com>
2019-11-08 17:29:43 +0100

    ultimos cambios

--- ud2_propuestos/26ObtenerDatos.php
+++ ud2_propuestos/26ObtenerDatos.php
@@ -23,5 +23,20 @@ function obtenerNavegador($user_agent){
         return 'No hemos podido detectar su navegador';
 }
 
-echo 'Navegador: ' . $navegador = obtenerNavegador($user_agent);
+echo '1. Navegador: <b>' . $navegador = obtenerNavegador($user_agent) . '</b><br><br>';
 
+echo '2. La version de PHP es: <b>' . phpversion() . '</b><br><br>';
+
+echo '3. Protocolo: <b>' . $_SERVER['SERVER_PROTOCOL'] . '</b><br>';
+echo 'Nombre del Host: <b>' . $_SERVER['SERVER_NAME'] . '</b><br>';
+echo 'Path: <b>' . $_SERVER['SCRIPT_NAME'] . '</b><br><br>';
+
+echo '4. IP Cliente: <b>' . $_SERVER['HTTP_HOST'] . '</b><br>';
+echo 'IP Servidor: <b>' . $_SERVER['REMOTE_ADDR'] . '</b><br>';
+echo 'Nombre del archivo: <b>' . basename($_SERVER['PHP_SELF']) . '</b><br><br>';
+
+echo '5. Propietario del archivo: <b>';
+$usuario = posix_getpwuid(fileowner(basename($_SERVER['PHP_SELF'])));
+echo $usuario['name'] . '</b><br><br>';
+
+echo '6. Url actual: <b>' . $url = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] . '</b>';
