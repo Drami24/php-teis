9d2fa0c99310528a9064866f28966b05232270dd
damianld <damilores@gmail.com>
2019-11-13 16:53:22 +0100

    formatear texto

--- ud2_propuestos/22EliminarCeros.php
+++ ud2_propuestos/22EliminarCeros.php
@@ -3,7 +3,7 @@
 $numero = "000327023.24";
 if (is_numeric($numero)){
     echo "Sin quitar: $numero <br>";
-    echo "Final: " . ltrim($numero, 0);
+    echo "Final: " . ltrim($numero, '0');
 } else {
     echo 'El texto no es un numero';
 }
