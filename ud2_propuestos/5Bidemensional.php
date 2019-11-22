ae417aad2754a65a8dd6390aefe1884f808fadb9
damianld <damilores@gmail.com>
2019-11-22 17:39:01 +0100

    subida

--- ud2_propuestos/5Bidemensional.php
+++ ud2_propuestos/5Bidemensional.php
@@ -11,10 +11,17 @@ for ($i = 0; $i < 8; $i++) {
 }
 
 for ($i = 0; $i < 8; $i++) {
+    for ($j = 0; $j < 8; $j++) {
+        echo $matriz[$i][$j] . ' ';
+    }
+    echo '<br>';
+}
+
+echo '<br>';
+for ($i = 0; $i < 8; $i++) {
     echo 'fila' . $i . '<br>';
     for ($j = 0; $j < 8; $j++) {
         echo 'posicion: ' . $matriz[$i][$j];
-        //echo $matriz[$i][$j];
         echo '<br>';
     }
     echo '<br>';
