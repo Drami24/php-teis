a2955af9e3098ce5f855ae8cd54c23988e47104b
damianld <damilores@gmail.com>
2020-01-10 17:38:52 +0100

    subir cambios

--- ud3_ejercicios/ejercicio2/Persoa.php
+++ ud3_ejercicios/ejercicio2/Persoa.php
@@ -1,42 +1,73 @@
 <?php
 
-class Persoa
+class Persoa 
 {
     private $nome;
-    private $dataNacemento;
+    private DateTime $dataNacemento;
     private $sexo;
 
-    function __construct($nome, $dataNacemento, $sexo)
+    function __construct($nome, $dataNacemento, $sexo = 'H')
     {
         $this->nome = $nome;
         $this->dataNacemento = $dataNacemento;
         $this->sexo = $sexo;
     }
 
-    public function __set($atributo, $valor)
-    {
-        if (property_exists(__CLASS__, $atributo)) {
-            $this->$atributo = $valor;
+    function getNome() {
+        return $this->nome;
+    }
+
+    function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    function getDataNacemento() {
+        return $this->dataNacemento;
+    }
+
+    function setDataNacemento($dataNacemento) {
+        $this->dataNacemento = $dataNacemento;
+    }
+
+    function getSexo() {
+        return $this->sexo;
+    }
+
+    function setSexo($sexo) {
+        $this->sexo = $sexo;
+    } 
+
+    public function mostrarPersoa($persoa) {
+        echo 'Nome: ' . $persoa->nome . '<br>';
+        //echo 'Fecha de nacemento: ' . $persoa->dataNacemento . '<br>';
+        echo 'Sexo: ';
+        if ($persoa->sexo == 'H') {
+            echo 'Hombre' . '<br>';
+        } else if ($persoa->sexo == 'M') {
+            echo 'Muller' . '<br>';
         } else {
-            echo "Non existe o atributo $atributo.";
+            echo 'Descoñecido <br>';
         }
-    }
-    
-    public function __get($atributo)
-    {
-        if (property_exists(__CLASS__, $atributo)) {
-            return $this->$atributo;
-        }
-        return NULL;
+        echo '<br>';
     }
 
-    public function mostrarPersoa($persona) {
-        echo $persona->nome . '<br>';
-        echo $persona->dataNacemento . '<br>';
-        echo $persona->sexo . '<br>';
+    public function diasVivo($persoa) {
+        $fechaNacemento = new DateTime("1994-11-24");
+        $fechaActual = new DateTime("now");
+        //$fechaProba = $persoa->getDataNacemento;
+        $diasVivo = $fechaNacemento->diff($fechaActual);
 
-
+        echo $diasVivo->days . ' days ' . '<br>';
     }
+
 }
-$persona1 = new Persoa("Sara", "Plaza", "Ferreiro");
-mostrarPersoa($persona1);
+
+$persoa1 = new Persoa('Sara', new DateTime("1994-11-24"), 'M');
+$persoa1->mostrarPersoa($persoa1);
+$persoa1->diasVivo($persoa1);
+
+$persoa2 = new Persoa('Damián', '1993-01-24');
+$persoa2->mostrarPersoa($persoa2);
+
+$persoa3 = new Persoa('Jose Ramón', '1994-10-16', 'Otro');
+$persoa3->mostrarPersoa($persoa3);
