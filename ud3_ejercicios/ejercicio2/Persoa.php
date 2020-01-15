bd4d1c030c8e9995334560981bb878e720450ff1
damianld <damilores@gmail.com>
2020-01-15 16:30:28 +0100

    correxir obxecto persoas

--- ud3_ejercicios/ejercicio2/Persoa.php
+++ ud3_ejercicios/ejercicio2/Persoa.php
@@ -37,43 +37,42 @@ class Persoa
         $this->sexo = $sexo;
     } 
 
-    public function mostrarPersoa($persoa) {
-        echo 'Nome: ' . $persoa->nome . '<br>';
-        echo 'Fecha de nacemento: ' . $persoa->dataNacemento->format("Y-m-d") . '<br>';
-        echo 'Sexo: ' . $persoa->mostrarSexo($persoa) . '<br><br>';
+    function mostrarPersoa() {
+        echo 'Nome: ' . $this->nome . '<br>';
+        echo 'Fecha de nacemento: ' . $this->dataNacemento->format("Y-m-d") . '<br>';
+        echo 'Sexo: ' . $this->mostrarSexo() . '<br><br>';
     }
 
-    public function mostrarSexo($persoa) {
-        if ($persoa->sexo == 'H') {
-            return 'Hombre';
-        } else if ($persoa->sexo == 'M') {
+    function mostrarSexo() {
+        if ($this->sexo === 'H') {
+            return 'Home';
+        } else if ($this->sexo === 'M') {
             return 'Muller';
         } else {
             return 'Descoñecido';
         }
     }
 
-    public function diasVivo($dataNacemento) {
+    function diasVivo() {
         $fechaActual = new DateTime("now");
-        $diasVivo = $fechaActual->diff($dataNacemento);
+        $diasVivo = $fechaActual->diff($this->dataNacemento);
         return $diasVivo->format("%Y anos, %M meses, %d dias, un total de %a días");
     }
 
-    public function mostrarMensaxe($persoa) {
-        echo $persoa->getNome() . ' ten ' . $persoa->diasVivo($persoa->getDataNacemento()) . '<br>';
-        echo 'O seu sexo é: ' . $persoa->mostrarSexo($persoa) . '<br><br>';
+    function mostrarMensaxe() {
+        echo $this->nome . ' ten ' . $this->diasVivo() . '<br>';
+        echo 'O seu sexo é: ' . $this->mostrarSexo() . '<br><br>';
     }
-
 }
 
 $persoa1 = new Persoa('Sara', new DateTime("1994-09-21"), 'M');
 $persoa2 = new Persoa('Damián', new DateTime("1993-01-24"));
 $persoa3 = new Persoa('Jose Ramón', new DateTime("1994-10-16"), 'Otro');
 
-$persoa1->mostrarPersoa($persoa1);
-$persoa2->mostrarPersoa($persoa2);
-$persoa3->mostrarPersoa($persoa3);
+$persoa1->mostrarPersoa();
+$persoa2->mostrarPersoa();
+$persoa3->mostrarPersoa();
 
-$persoa1->mostrarMensaxe($persoa1);
-$persoa2->mostrarMensaxe($persoa2);
-$persoa3->mostrarMensaxe($persoa3);
+$persoa1->mostrarMensaxe();
+$persoa2->mostrarMensaxe();
+$persoa3->mostrarMensaxe();
