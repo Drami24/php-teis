0b577939519a28d18836a5e3e658a3ef5bd339ed
damianld <damilores@gmail.com>
2020-02-18 17:16:16 +0100

    crear tabla productos

--- pedidos/Productos.php
+++ pedidos/Productos.php
@@ -14,13 +14,29 @@
 <body>
 <?php
     require 'Cabecera.php';
-    //$categoria = obtenerCategoria($_GET['categoria']);
-    $categoria = obtenerCategoria(2);
-    //var_dump($categoria);
-    //$productos = obtenerProductosCategoria($_GET['categoria']);
-    $productos = obtenerProductosCategoria(1);
-    var_dump($productos);
-
+    $categoria = obtenerCategoria($_GET['categoria']);
+    $productos = obtenerProductosCategoria($_GET['categoria']);
+    echo '<h1>' . $categoria['nombre'] . '</h1>';
+    echo '<p>' . $categoria['descripcion'] . '</p>';
+    echo '<table>';
+    echo '<tr><th>Nombre</th><th>Descripcion</th><th>Peso</th><th>Stock</th><th>Comprar</th><tr>';
+    foreach ($productos as $producto) {
+        $codigo = $producto->codigo;
+        $nombre = $producto->nombre;
+        $descripcion = $producto->descripcion;
+        $peso = $producto->peso;
+        $stock = $producto->stock;
+        echo "<tr><td>$nombre</td><td>$descripcion</td><td>$peso</td><td>$stock</td>
+        <td>
+            <form action='añadir.php' method='POST'>
+                <input name='unidades' type='number' min='1' value='1'>
+                <input type='submit' value='Comprar'>
+                <input name='codigo' type='hidden' value='codigo'>
+            </form>
+        </td>
+        </tr>";
+    }
+    echo '</table>';   
 ?>
 </body>
 </html>
